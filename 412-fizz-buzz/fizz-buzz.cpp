66d9bf129e7df5452c8b357039a9e90f01f2eba5
Vanshika Jain <vanjain020406@gmail.com>
2026-09-04 20:59:45 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (89.05%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.cpp
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<string> fizzBuzz(int n) {
         vector<string> answer;
-
+        answer.reserve(n);
         for (int i = 1; i <= n; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 answer.push_back("FizzBuzz");
